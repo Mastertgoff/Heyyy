f89c211f70883fba1f1822decde966e5c65649c8
Muhammedshifin <muhammedshifink4@gmail.com>
2023-02-22 16:36:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CyberTG/Queen-autofilter.git /master_personal
+  git clone https://github.com/Muhammedshifin/Heyyy.git /Heyyy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /master_personal
+  git clone $UPSTREAM_REPO /Heyyy
 fi
-cd /master_personal
+cd /Heyyy
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
