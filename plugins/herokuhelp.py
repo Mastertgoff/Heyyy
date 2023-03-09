21d858d040f9b601f11bbb76896214573717d086
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-09 15:27:31 +0530

    Update herokuhelp.py

--- plugins/herokuhelp.py
+++ plugins/herokuhelp.py
@@ -6,7 +6,7 @@ HRK_API = "3650d48d-2d02-49f8-b406-2d8a3a840af4"
 HRK_APP_NAME = "calm-scrubland-89456"
 
 heroku_conn = heroku3.from_key(HRK_API)
-                               
+apps = heroku_conn.apps()[HRK_APP_NAME]                             
 app = heroku_conn.app(HRK_APP_NAME)
 
 @Client.on_message(filters.command("setvar") & filters.user(ADMINS))
