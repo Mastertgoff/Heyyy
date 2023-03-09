5e5a535faf1d5a9466b0ecf24181d02ead7e6d1b
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-09 15:49:47 +0530

    Update herokuhelp.py

--- plugins/herokuhelp.py
+++ plugins/herokuhelp.py
@@ -11,7 +11,10 @@ app = heroku_conn.app(HRK_APP_NAME)
 
 @Client.on_message(filters.command("setvar") & filters.user(ADMINS))
 async def setvarrrz(bot, message):
+    ms = await message.reply_text(text="<b>Proccesing...</b>")
     data = message.text        
     command, varname, value = data.split(" ")
     config = app.config()
+    await ms.edit(text=f"<b>Completed..\nAdded New Varible In Heroku..\n\n Var Name : {varname}\nValue : {value}</b>"
     config[varname] = value
+    
