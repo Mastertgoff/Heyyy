d2aa010cbecd6bfcef5724dc0d45aefae620a7cd
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-09 15:22:03 +0530

    Update herokuhelp.py

--- plugins/herokuhelp.py
+++ plugins/herokuhelp.py
@@ -5,7 +5,7 @@ from info import ADMINS
 HRK_API = "3650d48d-2d02-49f8-b406-2d8a3a840af4"
 HRK_APP_NAME = "calm-scrubland-89456"
 
-heroku_conn = heroku3.from_key('HRK_API')
+heroku_conn = heroku3.from_key(HRK_API)
                                
 app = heroku_conn.app(HRK_APP_NAME)
 
