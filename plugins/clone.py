94de3decd9c876f9a8600e264d229e60dc1b6580
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-14 20:00:33 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -25,5 +25,5 @@ async def delvarrrssz(bot, message):
             'username': bot.username
         }
         await msg.edit_text(f"✅ The bot @{bot.username} is now working like Groups Guard.\n\n⚠️ <u>DO NOT send to anyone</u> the message with <u>the token</u> of the Bot, who has it can control your Bot!\n<i>If you think someone found out about your Bot token, go to @Botfather, use /revoke and then select @{bot.username}</i>")
-     except BaseException as e:
+    except BaseException as e:
         await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{e}</code>\n\n❔ Forward this message to @vionite to be fixed.")
