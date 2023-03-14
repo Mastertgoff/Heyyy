fb11362248fb9769f97f9c1b976b10b6b15d1432
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-14 15:21:46 +0530

    Create openai.py

---
+++ plugins/openai.py
@@ -0,0 +1,20 @@
+from urllib import response
+import openai
+from pyrogram import Client, filters, enums
+
+OPEN_AI_KEY = "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"
+
+def ai_responses(input_text):
+    user_message = str(input_text).lower()
+    prompt_addition = "ENTER YOUR PROMPT HERE" + user_message
+    openai.api_key = OPEN_AI_KEY
+    bot_output = openai.Completion.create(engine="text-davinci-002",prompt=prompt_addition,temperature=0.7,max_tokens=200,top_p=1,frequency_penalty=0,presence_penalty=0)
+    response = bot_output['choices'][0]['text']
+    return response
+  
+@Client.on_message(filters.command("openai") & filters.user(ADMINS))
+async def delvarrrssz(bot, message):
+    data = message.text        
+    command, query = data.split(" ")
+    response = resp.ai_responses(query)
+    await message.reply_text(response)
