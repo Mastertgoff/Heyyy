8474b58b0748000eed74bac5bf7b4e663b152fd5
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-15 09:46:18 +0530

    Update commands.py

--- clone_plugins/commands.py
+++ clone_plugins/commands.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from info import ADMINS
 
 @clone_bot.on_message(filters.user(ADMINS) & filters.command("start"))
 async def eval(client, message):
