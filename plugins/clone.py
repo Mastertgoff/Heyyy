71f7b1eca07ca28d43e053aca2aeed2c66c69393
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-03-23 09:59:48 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters, enums
 from info import ADMINS, API_ID, API_HASH
 import re
 from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
+from database.clone_botsdb import save_bot_details
 
 @Client.on_message(filters.command("clone") & filters.user(ADMINS))
 async def delvarrrssz(bot, message):
@@ -25,6 +26,9 @@ async def delvarrrssz(bot, message):
             'token': bot_token,
             'username': bot.username
         }
+        save_bot_details(details)
+
+        
         await msg.edit_text(f"✅ The bot @{bot.username} is now working like Groups Guard.\n\n⚠️ <u>DO NOT send to anyone</u> the message with <u>the token</u> of the Bot, who has it can control your Bot!\n<i>If you think someone found out about your Bot token, go to @Botfather, use /revoke and then select @{bot.username}</i>")
     except BaseException as e:
         await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{e}</code>\n\n❔ Forward this message to @Master_broi to be fixed.")
