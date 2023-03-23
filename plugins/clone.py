4e3a535492cc6887da06d21193877c60dd8e83bc
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-03-23 12:53:20 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -26,10 +26,11 @@ async def delvarrrssz(bot, message):
             'token': bot_token,
             'username': bot.username
         }
+        Bot = clone_bot
+        Bot.run()
+        #Nothing......
         await save_bot_details(details)       
         await msg.edit_text(f"✅ The bot @{bot.username} is now working like Groups Guard.\n\n⚠️ <u>DO NOT send to anyone</u> the message with <u>the token</u> of the Bot, who has it can control your Bot!\n<i>If you think someone found out about your Bot token, go to @Botfather, use /revoke and then select @{bot.username}</i>")
     except BaseException as e:
         await msg.edit_text(f"⚠️ <b>BOT ERROR:</b>\n\n<code>{e}</code>\n\n❔ Forward this message to @Master_broi to be fixed.")
 
-Bot = clone_bot
-Bot.run()
