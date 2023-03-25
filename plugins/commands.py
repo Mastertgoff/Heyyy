093a24dbb32152e0134fceba70391b8d4cbdfaf9
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-03-25 08:38:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -214,11 +214,11 @@ async def start(client, message):
             await asyncio.sleep(1) 
         return await sts.delete()
     
-    elif data.split("-", 1)[0] == "imdb":
+    elif data.split("-", 1)[0] == "#imdb":
         k = await message.reply('Searching ImDB')
-        cb_data = message.text
-        main_data = cb_data.replace(" ","-")
-        title = main_data.split("#")
+        #Rmcb
+        main_data = data.replace(" ","-")
+        imdb, title = main_data.split("#")
         await k.edit(f"Searching ImDB For {title}")
         movies = await get_poster(title, bulk=True)
         if not movies:
