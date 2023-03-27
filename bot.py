4b16bb4c7c0df928840bfac244e0082bf158a463
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-03-27 10:07:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ class Bot(Client):
         await self.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(today, time))
         bots = await cdb.get_all_bots()
         async for bot in bots:
-            await start_clone_bots(str(bot['token']))
+            await self.start_clone_bots(str(bot['token']))
 
     async def stop(self, *args):
         await super().stop()
