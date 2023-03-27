39e299bcda9495856431ca90e934a6551f229655
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-03-27 11:18:01 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -11,7 +11,7 @@ async def delvarrrssz(bot, message):
     user_id = message.from_user.id
     data = message.text
     command, bot_token = data.split(" ")
-    if await db.is_bot_exist(bot_token):
+    if db.is_bot_exist(bot_token):
         await msg.edit("This Bot Is aldready Running")
     else:
         try:
