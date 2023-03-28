4b6e1d3700f54b1ecf897fc30369f4e9990bd399
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-03-28 14:01:06 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -43,7 +43,7 @@ Free Space: <code>{free}</code> """
     
     
 @Client.on_message(filters.command("startclone") & filters.user(ADMINS))
-async def stop_button(bot, message):
+async def stop_buttnon(bot, message):
     msg = await bot.send_message(text="**🔄 𝙿𝚁𝙾𝙲𝙴𝚂𝚂𝙴𝚂 𝚂𝚃𝙾𝙿𝙴𝙳. 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙸𝙽𝙶...**", chat_id=message.chat.id)       
     await asyncio.sleep(3)
     await msg.edit("**✅️ 𝙱𝙾𝚃 𝙸𝚂 𝚁𝙴𝚂𝚃𝙰𝚁𝚃𝙴𝙳. 𝙽𝙾𝚆 𝚈𝙾𝚄 𝙲𝙰𝙽 𝚄𝚂𝙴 𝙼𝙴**")
