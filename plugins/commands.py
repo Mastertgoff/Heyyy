aa9f7fb7ad05b9b27f4efc886bd8049cde7bd1ea
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-04-02 10:09:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -168,6 +168,7 @@ async def start(client, message):
             await asyncio.sleep(1) 
         await sts.delete()
         return
+    
     elif data.split("-", 1)[0] == "DSTORE":
         sts = await message.reply("<b>Please wait...</b>")
         b_string = data.split("-", 1)[1]
