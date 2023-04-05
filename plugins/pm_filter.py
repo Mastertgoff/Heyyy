7f66b86cbeedc527f7df6625b1a402dc7f8ba249
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-04-05 09:45:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -17,7 +17,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings, get_shortlink, send_all
 from database.users_chats_db import db
-from database.ia_filterdb import Media, get_file_details, get_bad_files
+from database.ia_filterdb import Media, get_file_details, get_search_results, get_bad_files
 from database.filters_mdb import (
     del_all,
     find_filter,
